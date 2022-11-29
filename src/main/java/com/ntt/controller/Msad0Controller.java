890a8c1c8f57f04af60a028f2da105a208514670
Andrea Micaroni <andrea.micaroni@nttdata.com>
2022-11-29 15:43:18 +0100

    Creati eventi

--- src/main/java/com/ntt/controller/Msad0Controller.java
+++ src/main/java/com/ntt/controller/Msad0Controller.java
@@ -19,9 +19,46 @@ public class Msad0Controller {
 		this.service = service;
 	}
 	
-	@PostMapping
+	//delete
+	@PostMapping(path = "/delete")
 	public Payload save(@RequestBody Payload payload) {
-		return this.service.save(payload);
+		return this.service.delete(payload);
 	}
 	
+	//aggiunto io, path provvisori
+	@PostMapping(path = "/activate")
+	public Payload activate(@RequestBody Payload payload) {
+		return this.service.activate(payload);
+	}
+	
+	@PostMapping(path = "/docSignedOffered")
+	public Payload docSignedOffered(@RequestBody Payload payload) {
+		return this.service.docSignedOffered(payload);
+	}
+	
+	@PostMapping(path = "/partiallySignedOffered")
+	public Payload partiallySignedOffered(@RequestBody Payload payload) {
+		return this.service.partiallySignedOffered(payload);
+	}
+	
+	@PostMapping(path = "/firmaOfferSigned")
+	public Payload firmaOfferSigned(@RequestBody Payload payload) {
+		return this.service.firmaOfferSigned(payload);
+	}
+	
+	@PostMapping(path = "/offerSaved")
+	public Payload offerSaved(@RequestBody Payload payload) {
+		return this.service.offerSaved(payload);
+	}
+	
+	@PostMapping(path = "/offerReady")
+	public Payload offerReady(@RequestBody Payload payload) {
+		return this.service.offerReady(payload);
+	}
+	
+	@PostMapping(path = "/startSign")
+	public Payload startSign(@RequestBody Payload payload) {
+		return this.service.startSign(payload);
+	}
+
 }
--- src/main/java/com/ntt/service/Msad0EventsService.java
+++ src/main/java/com/ntt/service/Msad0EventsService.java
@@ -21,7 +21,30 @@ public class Msad0EventsService {
 
 	@Value("${topic.customer.name:SDH_SVIL_MSAD0_DOCUMENTALE_DELETED_V1}")
 	private String topicDocumentaleDelete;
+	
+	//aggiunte io
+	@Value("${topic.customer.name:SDH_SVIL_MSAD0_PRATICA_ACTIVATE_V1}")
+	private String topicPraticaActivate;
+	
+	@Value("${topic.customer.name:SDH_SVIL_MSAD0_DOCUMENTALE_SIGNED_OFFER_SAVED_V1}")
+	private String topicDocumentaleSignedOfferSaved;
+	
+	@Value("${topic.customer.name:SDH_SVIL_MSAD0_DOCUMENTALE_PARTIALLY_SIGNED_OFFER_SAVED_V1}")
+	private String topicDocumentalePartiallySignedOfferSaved;
+	
+	@Value("${topic.customer.name:SDH_SVIL_MSAD0_FIRMA_OFFER_SIGNED_V1}")
+	private String topicFirmaOfferSigned;
 
+	@Value("${topic.customer.name:SDH_SVIL_MSAD0_DOCUMENTALE_OFFER_SAVED_V1}")
+	private String topicDocumentaleOfferSaved;
+	
+	@Value("${topic.customer.name:SDH_SVIL_MSAD0_FIRMA_OFFER_READY_V1}")
+	private String topicFirmaOfferReady;	
+	
+	@Value("${topic.customer.name:SDH_SVIL_MSAD0_FIRMA_START_SIGN_V1}")
+	private String topicFirmaStartSign;
+	
+	
 	public void documentaleDelete(Payload payload) {
 		Msad0CreatedEvent event = new Msad0CreatedEvent();
 		event.setData(payload);
@@ -32,6 +55,91 @@ public class Msad0EventsService {
 		event.setIdSuperpratica(payload.getIdSuperpratica());
 		
 		this.producer.send(this.topicDocumentaleDelete, event);
+	}
+
+	//Aggiunte io 
+	public void praticaActivate(Payload payload) {
+		Msad0CreatedEvent event = new Msad0CreatedEvent();
+		event.setData(payload);
+		event.setId(UUID.randomUUID().toString());
+		event.setType(EventType.CREATED);
+		event.setDate(new Date());
+		event.setIdPratica(payload.getIdPratica());
+		event.setIdSuperpratica(payload.getIdSuperpratica());
+		
+		this.producer.send(this.topicPraticaActivate, event);
+	}
+	
+	public void documentaleSignedOfferSaved(Payload payload) {
+		Msad0CreatedEvent event = new Msad0CreatedEvent();
+		event.setData(payload);
+		event.setId(UUID.randomUUID().toString());
+		event.setType(EventType.CREATED);
+		event.setDate(new Date());
+		event.setIdPratica(payload.getIdPratica());
+		event.setIdSuperpratica(payload.getIdSuperpratica());
+		
+		this.producer.send(this.topicDocumentaleSignedOfferSaved, event);
+	}
+	
+	public void documentalePartiallySignedOfferSaved(Payload payload) {
+		Msad0CreatedEvent event = new Msad0CreatedEvent();
+		event.setData(payload);
+		event.setId(UUID.randomUUID().toString());
+		event.setType(EventType.CREATED);
+		event.setDate(new Date());
+		event.setIdPratica(payload.getIdPratica());
+		event.setIdSuperpratica(payload.getIdSuperpratica());
+		
+		this.producer.send(this.topicDocumentalePartiallySignedOfferSaved, event);
+	}
+	
+	public void firmaOfferSigned(Payload payload) {
+		Msad0CreatedEvent event = new Msad0CreatedEvent();
+		event.setData(payload);
+		event.setId(UUID.randomUUID().toString());
+		event.setType(EventType.CREATED);
+		event.setDate(new Date());
+		event.setIdPratica(payload.getIdPratica());
+		event.setIdSuperpratica(payload.getIdSuperpratica());
+		
+		this.producer.send(this.topicFirmaOfferSigned, event);
+	}
+	
+	public void documentaleOfferSaved(Payload payload) {
+		Msad0CreatedEvent event = new Msad0CreatedEvent();
+		event.setData(payload);
+		event.setId(UUID.randomUUID().toString());
+		event.setType(EventType.CREATED);
+		event.setDate(new Date());
+		event.setIdPratica(payload.getIdPratica());
+		event.setIdSuperpratica(payload.getIdSuperpratica());
+		
+		this.producer.send(this.topicDocumentaleOfferSaved, event);
+	}
+	
+	public void firmaOfferReady(Payload payload) {
+		Msad0CreatedEvent event = new Msad0CreatedEvent();
+		event.setData(payload);
+		event.setId(UUID.randomUUID().toString());
+		event.setType(EventType.CREATED);
+		event.setDate(new Date());
+		event.setIdPratica(payload.getIdPratica());
+		event.setIdSuperpratica(payload.getIdSuperpratica());
+		
+		this.producer.send(this.topicFirmaOfferReady, event);
+	}
+	
+	public void firmaStartSign(Payload payload) {
+		Msad0CreatedEvent event = new Msad0CreatedEvent();
+		event.setData(payload);
+		event.setId(UUID.randomUUID().toString());
+		event.setType(EventType.CREATED);
+		event.setDate(new Date());
+		event.setIdPratica(payload.getIdPratica());
+		event.setIdSuperpratica(payload.getIdSuperpratica());
+		
+		this.producer.send(this.topicFirmaStartSign, event);
 	}
 
 }
